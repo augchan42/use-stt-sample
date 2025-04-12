c843480d687d319f343e93e843b197a6427506d1
Augustin Chan <aug@iterative.day>
2025-04-12 13:21:41 +0800

    Update typeRoots in tsconfig.json to reference src/@types for improved type definitions

---
+++ src/@types/lamejs/index.d.ts
@@ -0,0 +1,10 @@
+declare module 'lamejs' {
+  class Mp3Encoder {
+    constructor(channels: number, sampleRate: number, kbps: number);
+    encodeBuffer(buffer: Int16Array): Uint8Array;
+    flush(): Uint8Array;
+  }
+
+  const lamejs: { Mp3Encoder: typeof Mp3Encoder };
+  export default lamejs;
+} 
